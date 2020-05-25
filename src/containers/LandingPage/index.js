1310b7fdaa5cf601d48bfe181dac957b0bddb0a1
Karthik <karthik@Karthiks-Home-MacBook-Pro.local>
2020-05-25 00:00:56 -0700

    template for react-d3

---
+++ src/containers/LandingPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from 'reflexbox'
+
+const LandingPage = () => {
+  return (
+    <Box ml={5} mt={5}>
+        An amazing app template!
+    </Box>
+  )
+}
+
+export default LandingPage
